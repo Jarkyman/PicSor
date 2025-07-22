c6f908009e7426504701ffff078aa5fb1e5eb85f
Jackie Jensen <Jackiehartvig@hotmail.com>
2025-07-22 12:31:48 +0200

    Onboarding: Add intro screen explaining PicSor swipe features and flow

--- lib/main_preview.dart
+++ lib/main_preview.dart
@@ -1,22 +1,4 @@
 import 'package:flutter/material.dart';
-import 'screens/onboarding/photo_permission_screen.dart';
+import 'screens/onboarding/intro_screen.dart';
 
-void main() {
-  runApp(const PreviewApp());
-}
-
-class PreviewApp extends StatelessWidget {
-  const PreviewApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Preview',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: PhotoPermissionScreen(onNext: () {}),
-    );
-  }
-}
+void main() => runApp(MaterialApp(home: IntroScreen(onContinue: () {})));
