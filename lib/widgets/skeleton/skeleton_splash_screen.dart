117cb7852bc4087f37794832c8547db699f02920
Jackie Jensen <Jackiehartvig@hotmail.com>
2025-07-25 22:32:50 +0200

    Improve splash screen with proper loading indicator
    - Added animated app icon with pulsing primary color
    - Replaced skeleton elements with real CircularProgressIndicator
    - Added 'Loading your gallery...' text in primary color
    - Improved user experience with smooth 2-second animation
    - Splash screen now looks professional and shows actual loading state

--- lib/widgets/skeleton/skeleton_splash_screen.dart
+++ lib/widgets/skeleton/skeleton_splash_screen.dart
@@ -1,8 +1,36 @@
 import 'package:flutter/material.dart';
 import '../../core/theme.dart';
 
-class SkeletonSplashScreen extends StatelessWidget {
+class SkeletonSplashScreen extends StatefulWidget {
   const SkeletonSplashScreen({super.key});
+
+  @override
+  State<SkeletonSplashScreen> createState() => _SkeletonSplashScreenState();
+}
+
+class _SkeletonSplashScreenState extends State<SkeletonSplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      duration: const Duration(milliseconds: 2000),
+      vsync: this,
+    );
+    _animation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.easeInOut),
+    );
+    _animationController.repeat(reverse: true);
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,56 +39,57 @@ class SkeletonSplashScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            _buildSkeletonIcon(context),
+            _buildAppIcon(context),
             SizedBox(height: AppSpacing.xl),
-            _buildSkeletonSpinner(context),
+            _buildLoadingSpinner(context),
             SizedBox(height: AppSpacing.lg),
-            _buildSkeletonText(context),
+            _buildLoadingText(context),
           ],
         ),
       ),
     );
   }
 
-  Widget _buildSkeletonIcon(BuildContext context) {
-    return Container(
-      width: Scale.of(context, 72),
-      height: Scale.of(context, 72),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(Scale.of(context, 36)),
-        color:
-            Theme.of(context).brightness == Brightness.light
-                ? Colors.grey[300]
-                : Colors.grey[700],
-      ),
+  Widget _buildAppIcon(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _animation,
+      builder: (context, child) {
+        return Container(
+          width: Scale.of(context, 72),
+          height: Scale.of(context, 72),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(Scale.of(context, 36)),
+            color: Color.lerp(
+              AppColors.primary,
+              AppColors.primary.withValues(alpha: 0.7),
+              _animation.value,
+            ),
+          ),
+          child: Icon(
+            Icons.photo_library_outlined,
+            color: Colors.white,
+            size: Scale.of(context, 36),
+          ),
+        );
+      },
     );
   }
 
-  Widget _buildSkeletonSpinner(BuildContext context) {
-    return Container(
+  Widget _buildLoadingSpinner(BuildContext context) {
+    return SizedBox(
       width: Scale.of(context, 24),
       height: Scale.of(context, 24),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(Scale.of(context, 12)),
-        color:
-            Theme.of(context).brightness == Brightness.light
-                ? Colors.grey[300]
-                : Colors.grey[700],
+      child: CircularProgressIndicator(
+        strokeWidth: 2,
+        valueColor: AlwaysStoppedAnimation<Color>(AppColors.primary),
       ),
     );
   }
 
-  Widget _buildSkeletonText(BuildContext context) {
-    return Container(
-      width: Scale.of(context, 200),
-      height: Scale.of(context, 20),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(Scale.of(context, 10)),
-        color:
-            Theme.of(context).brightness == Brightness.light
-                ? Colors.grey[300]
-                : Colors.grey[700],
-      ),
+  Widget _buildLoadingText(BuildContext context) {
+    return Text(
+      'Loading your gallery...',
+      style: AppTextStyles.title(context).copyWith(color: AppColors.primary),
     );
   }
 }
